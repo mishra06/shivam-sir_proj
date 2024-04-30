0e34a9f4c12bc8c5ed306999f019b6647d69b924
mishra06 <mishra.vibhakar7262@gmail.com>
2024-04-30 21:20:42 +0530

    image added

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-
 import "./LoginForm.css";
+import SCHOOLIMG from "../assets/Students walking in front of university flat vector illustration.jpg";
 
 const LoginForm = () => {
   const [userType, setUserType] = useState("admin");
@@ -27,51 +27,86 @@ const LoginForm = () => {
   };
 
   return (
+    <div className="pages">
+      <div className="left_sec">
     <div className="container">
-        
-      
-        <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <div class="title selector">Login</div>
         <div className="selector">
-            <select value={userType} onChange={userTypeChange}>
-              <option value="admin">Admin</option>
-              <option value="parent">Parent</option>
-            </select>
-        </div>  
-          
-          {userType === "admin" ? (
-            <div className="admin_sec">
-             
-             <div class="input-box underline">
-                <input type="text" value={userId} placeholder="Enter Your Email" onChange={(e)=>setUserId(e.target.value)} required/>
-                <div class="underline"></div>
-                </div>
-                <div class="input-box">
-                    <input type="password" value={adminPassword} onChange={(e)=>setAdminPassword(e.target.value)} placeholder="Enter Your Password" required/>
-                    <div class="underline"></div>
-                </div>
-            </div>
-          ) : (
-            <>
-              <div class="input-box underline">
-                <input type="text" placeholder="Enter Your Email" value={userName} onChange={(e)=>setUserName(e.target.value)} required/>
-                    <div class="underline"></div>
-                </div>
-                <div class="input-box">
-                    <input type="password" placeholder="Enter Your Password" value={password} onChange={(e)=>setPassword(e.target.value)} required/>
-                    <div class="underline"></div>
-                </div>
-                <div class="input-box">
-                    <input type="text" placeholder="Enter Your Number" value={mobileNumber} onChange={(e)=>setMobileNumber(e.target.value)} required/>
-                    <div class="underline"></div>
-                </div>
-            </>
-          )}
-          <div class="input-box button">
-          <input type="submit" name="" value="Continue"/>
+          <select value={userType} onChange={userTypeChange}>
+            <option value="admin">Admin</option>
+            <option value="parent">Parent</option>
+          </select>
         </div>
-        </form>
-      
+
+        {userType === "admin" ? (
+          <div className="admin_sec">
+            <div class="input-box underline">
+              <input
+                type="text"
+                value={userId}
+                placeholder="Enter Your Email"
+                onChange={(e) => setUserId(e.target.value)}
+                required
+              />
+              <div class="underline"></div>
+            </div>
+            <div class="input-box">
+              <input
+                type="password"
+                value={adminPassword}
+                onChange={(e) => setAdminPassword(e.target.value)}
+                placeholder="Enter Your Password"
+                required
+              />
+              <div class="underline"></div>
+            </div>
+          </div>
+        ) : (
+          <>
+            <div class="input-box underline">
+              <input
+                type="text"
+                placeholder="Enter Your Email"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+                required
+              />
+              <div class="underline"></div>
+            </div>
+            <div class="input-box">
+              <input
+                type="password"
+                placeholder="Enter Your Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <div class="underline"></div>
+            </div>
+            <div class="input-box">
+              <input
+                type="text"
+                placeholder="Enter Your Number"
+                value={mobileNumber}
+                onChange={(e) => setMobileNumber(e.target.value)}
+                required
+              />
+              <div class="underline"></div>
+            </div>
+          </>
+        )}
+        <div class="input-box button">
+          <input type="submit" name="" value="Continue" />
+        </div>
+      </form>
+    </div>
+    </div>
+    <div className="right_sec">
+        <div className="side_img">
+          <img src={SCHOOLIMG}  alt="" />
+        </div>
+      </div>
     </div>
   );
 };
